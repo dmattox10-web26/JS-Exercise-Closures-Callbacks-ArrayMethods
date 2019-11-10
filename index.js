31e5ec5ba6c810800b9071e13ac6d56831fc4a13
Daniel Mattox <dmattox10@gmail.com>
2019-11-10 17:04:22 -0500

    All tests including stretch are passing

--- index.js
+++ index.js
@@ -166,8 +166,14 @@ function processContains(item, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  let deDuped = []
+  for (let i = 0; i < list.length; i++) {
+    if (deDuped.indexOf(list[i]) === -1) {
+      deDuped.push(list[i])
+    } 
+  }
+  return callback(deDuped)
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
